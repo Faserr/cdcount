0a205465ee3878be15107fed199b9391c94fa145
Faserr <46740335+Faserr@users.noreply.github.com>
2021-05-02 11:43:36 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+﻿//alert("测试");
+
+
+
+/*
+  var text = document.getElementsByClassName("edit")[0].value;
+  alert(text);
+*/
+
+function count_onClick(){
+  var text = $(".edit").val();
+  var arrText = text.split("\n");
+
+  var sum = 0
+  arrText.forEach(function(v){
+    //var mNum = v.replace("元", '');
+    var num = parseFloat(v)
+    if(num){
+      sum = sum + num;
+     // console.log("oks " + num);
+    }else{
+     // console.log("not " + num);
+    }
+  });
+
+  alert(sum);
+
+}
+
+function clear_onClick(){
+  $(".edit").val("");
+}
